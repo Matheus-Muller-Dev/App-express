52d81702e61eaf03ce28a049e49c2229002b48ef
Matheus Muller <matheusmiiller44@gmail.com>
2024-12-14 14:08:51 -0300

    Criando diretorio public para usar pages com arquivos html

--- home.js
+++ home.js
@@ -1,25 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-
-app.get('/home', (req, res) => {
-    res.send('<h1>Welcome to the home page</h1><p>This is the home page</p>');
-});
-
-app.get('/about', (req, res) => {
-    res.send('<h1>ABout us</h1><p>This my page</p>');
-});
-
-app.get('/contact', (req, res) => {
-    res.send('<h1>Contact us</h1><p>my e-mail: matheusmiiller44@gmail.com</p>');
-});
-
-app.use((req, res) => {
-    res.status(404).send('<h1>404 Not Found</h1>')
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+    console.log(`Server is running on http://localhost:${port}`)
+})
